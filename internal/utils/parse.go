d1ae06d3bff443315250a181f193d0fae2cdff06
Samuel Jędrzejewski <samijedrzejewski@gmail.com>
2026-09-17 13:08:11 +0200

    fix docker size parsing

--- internal/utils/parse.go
+++ internal/utils/parse.go
@@ -8,18 +8,21 @@ import (
 
 func ParseByteString(s string) int64 {
 	re := regexp.MustCompile(`([\d\.]+)\s*(B|KB|MB|GB|TB)`)
-	matches := re.FindStringSubmatch(strings.ToUpper(s))
-	if len(matches) < 3 {
+	
+	matches := re.FindAllStringSubmatch(strings.ToUpper(s), -1)
+	if len(matches) == 0 {
 		return 0
 	}
 
-	val, err := strconv.ParseFloat(matches[1], 64)
+	lastMatch := matches[len(matches)-1]
+
+	val, err := strconv.ParseFloat(lastMatch[1], 64)
 	if err != nil {
 		return 0
 	}
 
 	multiplier := float64(1)
-	switch matches[2] {
+	switch lastMatch[2] {
 	case "KB":
 		multiplier = 1024
 	case "MB":
