fc92afe6c4be0f29156ebe6053a344f169ddeaea
Samuel Jędrzejewski <samijedrzejewski@gmail.com>
2026-09-18 20:00:31 +0200

    remove unnecessary cleaners load

--- cmd/devcull/main.go
+++ cmd/devcull/main.go
@@ -1,20 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
-
-	"github.com/238SAMIxD/devcull/internal/cleaner"
-	"github.com/238SAMIxD/devcull/internal/plugin"
 )
 
 func main() {
-	nativeCleaners := cleaner.Native()
-	pluginCleaners := plugin.LoadPlugins()
-	allCleaners := append(nativeCleaners, pluginCleaners...)
-
-	fmt.Printf("Successfully loaded %d cleaners!\n", len(allCleaners))
-
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
