0d64c1d3a1bbbee3db3a848063f921acff1eb707
Samuel Jędrzejewski <samijedrzejewski@gmail.com>
2026-09-18 14:51:16 +0200

    fix regex

--- internal/utils/parse.go
+++ internal/utils/parse.go
@@ -6,10 +6,10 @@ import (
 	"strings"
 )
 
+var byteRe = regexp.MustCompile(`(?i)([\d.]+)\s*(B|KB|MB|GB|TB)`)
+
 func ParseByteString(s string) int64 {
-	re := regexp.MustCompile(`([\d\.]+)\s*(B|KB|MB|GB|TB)`)
-	
-	matches := re.FindAllStringSubmatch(strings.ToUpper(s), -1)
+	matches := byteRe.FindAllStringSubmatch(s, -1)
 	if len(matches) == 0 {
 		return 0
 	}
@@ -22,7 +22,7 @@ func ParseByteString(s string) int64 {
 	}
 
 	multiplier := float64(1)
-	switch lastMatch[2] {
+	switch strings.ToUpper(lastMatch[2]) {
 	case "KB":
 		multiplier = 1024
 	case "MB":
